97da2708d4afe65ca1995b651e728f2b925213bf
David Cantu <dacantu@microsoft.com>
2020-11-13 02:00:46 -0800

    Fix TheoryWindowsOnlyAttribute

--- tests/Helpers/TheoryWindowsOnlyAttribute.cs
+++ tests/Helpers/TheoryWindowsOnlyAttribute.cs
@@ -5,7 +5,7 @@ namespace GlobbingTests.Helpers
     public class TheoryWindowsOnlyAttribute : TheoryAttribute
     {
 #if NOT_WINDOWS
-        public FactWindowsOnlyAttribute() 
+        public TheoryWindowsOnlyAttribute() 
         {
             Skip = "Ignored on other than Windows";
         }
