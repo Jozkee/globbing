ed44ac2eeae07f7acf8b6174ac27a4592156bf3b
David Cantu <dacantu@microsoft.com>
2020-11-12 17:23:47 -0800

    Source clean-up

--- src/GlobbingEnumerable.cs
+++ src/GlobbingEnumerable.cs
@@ -8,123 +8,112 @@ namespace Globbing
 {
     public class GlobbingEnumerable : FileSystemEnumerable<string>
     {
+        private static readonly char s_directorySeparator = Path.DirectorySeparatorChar;
         private readonly Glob _glob;
         private bool _patternIsRooted;
 
         // RecurseSubdirectories must be turned on to properly 
         // support patterns with globstar and/or with subdirectories.
-        private static readonly EnumerationOptions s_options 
+        private static readonly EnumerationOptions s_recurseSubdirectoriesOptions 
             = new EnumerationOptions { RecurseSubdirectories = true };
 
         public GlobbingEnumerable(string directory, string pattern)
-        : base(directory, Transform, s_options)
+        : base(directory, Transform, GetOptions())
         {
             _patternIsRooted = Path.IsPathRooted(pattern);
             _glob = Glob.Parse(pattern);
 
-            ShouldIncludePredicate = IsMatchV2;
+            ShouldIncludePredicate = IsMatch;
         }
 
         private static string Transform(ref FileSystemEntry entry) => entry.ToFullPath();
 
-        private bool IsMatch(ref FileSystemEntry entry)
-            {
-            // To get the relative path that we will try to match with the glob pattern, 
-            // we will use (Directory - RootDirectory) + Filename.
-            ReadOnlySpan<char> pathMinusRoot = entry.Directory.Slice(entry.RootDirectory.Length);
-            int pathMinusRootLength = pathMinusRoot.Length;
-
-            // TODO: Use ArrayPool instead after a certain threshold
-            // e.g: after 1024 chanracters.
-            Span<char> fullPath = stackalloc char[pathMinusRootLength + entry.FileName.Length];
-
-            if (pathMinusRootLength > 0)
-            {
-                // Skip '\\'.
-                pathMinusRoot.Slice(1).CopyTo(fullPath);
-
-                // Append trailing '\\' before filename.
-                fullPath[pathMinusRootLength - 1] = '\\';
-            }
-
-            entry.FileName.CopyTo(fullPath.Slice(pathMinusRootLength));
-
-            return _glob.IsMatch(fullPath);
+        private static EnumerationOptions GetOptions()
+        {
+            // TODO: avoid using s_recurseSubdirectoriesOptions if the pattern is limited to one level i.e: doesn't require recursing subdirectories.
+            //e.g: "foo", "./foo".
+            return s_recurseSubdirectoriesOptions;
         }
 
-        private bool IsMatchV2(ref FileSystemEntry entry)
+        private bool IsMatch(ref FileSystemEntry entry)
         {
-            // if the pattern is rooted (e.g: C:/**/bar.cs), we have to match against entry's absolute path.
-            if (_patternIsRooted)
+            return _patternIsRooted ?
+                // if the pattern is rooted (e.g: C:/**/bar.cs), we have to match against entry's absolute path.
+                IsMatchAbsolutePattern(ref entry) :
+                // if the pattern is NOT rooted (e.g: **/foo.cs), we have to match against entry's relative path.
+                IsMatchRelativePattern(ref entry);
+        }
+
+        private bool IsMatchAbsolutePattern(ref FileSystemEntry entry)
+        {
+            ReadOnlySpan<char> directory = entry.Directory;
+            ReadOnlySpan<char> filename = entry.FileName;
+
+            int directoryPathLength = directory.Length;
+
+            // Unlike non-rooted phats, a rooted path may contain a trailing separator e.g: C:\
+            // do not append a separator in that case.
+            if (!directory.EndsWith(s_directorySeparator))
             {
-                ReadOnlySpan<char> directory = entry.Directory;
-                ReadOnlySpan<char> filename = entry.FileName;
-
-                int directoryLength = directory.Length;
-
-                // Unlike non-rooted phats, 'C:\' already contains a trailing '\'.
-                // Do not append '\' for that case.
-                if (directory[directory.Length - 1] != '\\')
-                {
-                    directoryLength++;
-                }
-
-                int absolutePathLength = directoryLength + filename.Length;
-
-                // TODO: Use ArrayPool instead after a certain threshold
-                // e.g: after 1024 chanracters.
-                Span<char> fullPathAbsolute = stackalloc char[absolutePathLength];
-
-                directory.CopyTo(fullPathAbsolute);
-                fullPathAbsolute[directory.Length] = '\\';
-                filename.CopyTo(fullPathAbsolute.Slice(directoryLength));
-
-                return _glob.IsMatch(fullPathAbsolute);
+                directoryPathLength++;
             }
-            // if the pattern is NOT rooted (e.g: **/foo.cs), we have to match against entry's relative path.
+
+            int absolutePathLength = directoryPathLength + filename.Length;
+
+            // TODO: Consider using ArrayPool after the 1024 threshold.
+            Span<char> entryFullPath = absolutePathLength > 1024 ? new char[absolutePathLength] : stackalloc char[absolutePathLength];
+
+            directory.CopyTo(entryFullPath);
+            entryFullPath[directory.Length] = s_directorySeparator;
+            filename.CopyTo(entryFullPath.Slice(directoryPathLength));
+
+            return _glob.IsMatch(entryFullPath);
+
+        }
+
+        private bool IsMatchRelativePattern(ref FileSystemEntry entry)
+        {
+            ReadOnlySpan<char> rootDirectory = entry.RootDirectory;
+
+            // Sustract the root directory to get the directory's relative path from the specified directory in the ctor.
+            int rootDirectoryLength = rootDirectory.Length;
+
+            // For consistency:
+            // If RootDirectory ends with a separator e.g: C:\, we decrease the lenght by one
+            // since we want to keep a leading separator in directoryPathRelative.
+            if (rootDirectory.EndsWith(s_directorySeparator))
+            {
+                rootDirectoryLength--;
+            }
+
+            ReadOnlySpan<char> directoryPathRelative = entry.Directory.Slice(rootDirectoryLength);
+
+            // If length is 0, we are in a non-rooted root directory.
+            // If length is 1, we ara in a rooted root directory and the separator in directory[0] is just a side-effect of normalizing the relative path in above step.
+            // In either case, entry is in the root directory and no path needs to be appended.
+            // Note: the "root directory" is the same as the directory specified in the ctor.
+            if (directoryPathRelative.Length > 1)
+            {
+                Debug.Assert(directoryPathRelative[0] == s_directorySeparator);
+
+                int relativePathLength = directoryPathRelative.Length + entry.FileName.Length;
+                // TODO: Consider using ArrayPool after the 1024 threshold.
+                Span<char> entryFullPathRelative = relativePathLength > 1024 ? new char[relativePathLength] : stackalloc char[relativePathLength];
+
+                // Skip directory's leading separator.
+                directoryPathRelative.Slice(1).CopyTo(entryFullPathRelative);
+
+                // Append trailing separator before filename.
+                entryFullPathRelative[directoryPathRelative.Length - 1] = s_directorySeparator;
+
+                entry.FileName.CopyTo(entryFullPathRelative.Slice(directoryPathRelative.Length));
+
+                return _glob.IsMatch(entryFullPathRelative);
+            }
             else
             {
-                ReadOnlySpan<char> rootDirectory = entry.RootDirectory;
-                int rootDirectoryLength = rootDirectory.Length;
-
-                // Normalization:
-                // If the root ends with '\', we won't count for that trailing '\' 
-                // since we need one slot to append '\' between directory and filename.
-                if (rootDirectory[rootDirectory.Length - 1] == '\\')
-                {
-                    rootDirectoryLength--;
-                }
-
-                // Remove the root directory to keep the relative path from the specified directory in the ctor.
-                ReadOnlySpan<char> directory = entry.Directory.Slice(rootDirectoryLength);
-
-                if (directory.Length > 1)
-                {
-                    Debug.Assert(directory[0] == '\\');
-
-                    // TODO: Use ArrayPool instead after a certain threshold
-                    // e.g: after 1024 chanracters.
-                    Span<char> fullPathRelative = stackalloc char[directory.Length + entry.FileName.Length];
-
-                    // Skip directory's leading '\\'.
-                    directory.Slice(1).CopyTo(fullPathRelative);
-
-                    // Append trailing '\\' before filename.
-                    fullPathRelative[directory.Length - 1] = '\\';
-
-                    entry.FileName.CopyTo(fullPathRelative.Slice(directory.Length));
-
-                    return _glob.IsMatch(fullPathRelative);
-                }
-                // if directory.Length == 0, we are in a non-rooted root directory.
-                // if directory.Length == 1, we ara in a rooted root directory and the '\' in directory[0] is just a side-effect of normalizing the way we get the relative path.
-                // in either case, the entry is in the root directory and no path needs to be appended.
-                // Note: the "root directory" is the same as the directory specified in the ctor.
-                else
-                {
-                    return _glob.IsMatch(entry.FileName);
-                }
+                return _glob.IsMatch(entry.FileName);
             }
-        } 
+        }
     }
 }
---
+++ src/GlobbingHelpers.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Globbing
+{
+    internal static class GlobbingHelpers
+    {
+        public static bool EndsWith(this ReadOnlySpan<char> span, char value)
+        {
+            Debug.Assert(span.Length > 0);
+            return span[span.Length - 1] == value;
+        }
+    }
+}
